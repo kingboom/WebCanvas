a47930e8ae6371017e5944a12af7d0902a86f7d7
kingboom <image104@gmail.com>
2013-08-07 09:42:35 +0900

    PhoneGap 카메라 및 Canvas 샘플

---
+++ src/org/androidtown/gap/canvas/MainActivity.java
@@ -0,0 +1,27 @@
+package org.androidtown.gap.canvas;
+
+import org.androidtown.gap.canvas.R;
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
+        
+        super.loadUrl("file:///android_asset/www/index.html", 1500);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
